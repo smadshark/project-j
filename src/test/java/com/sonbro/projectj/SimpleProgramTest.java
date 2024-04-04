5f12c1f6ef3008237d15244b93cfb96b9de88c22
Sg <smadshark@gmail.com>
2024-04-05 00:57:07 +0900

    add test controller

--- src/test/java/com/sonbro/projectj/SimpleProgramTest.java
+++ src/test/java/com/sonbro/projectj/SimpleProgramTest.java
@@ -2,6 +2,7 @@ package com.sonbro.projectj;
 
 import org.junit.jupiter.api.Test;
 
+
 public class SimpleProgramTest {
     @Test
     public void textBlock() {
@@ -15,5 +16,27 @@ public class SimpleProgramTest {
                 good""";
 
         System.out.println(str2);
+    }
+
+    @Test
+    public void textBlockFormatted() {
+        String textBlock = """
+                {
+                  "id": %d,
+                  "name": %s
+                }
+                """.formatted(11, "goods");
+
+        System.out.println(textBlock);
+    }
+
+    @Test
+    public void recordTest() {
+        Item juice = new Item("juice", 3000);
+        System.out.println(juice.name);
+        System.out.println(juice.name());
+    }
+
+    record Item(String name, int price) {
     }
 }
