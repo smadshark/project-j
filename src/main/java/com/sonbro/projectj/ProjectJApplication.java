82b4bd70da1db124c6632bdcaf3d43d55ccd1a93
Sg <smadshark@gmail.com>
2024-04-05 00:31:22 +0900

    reset project

---
+++ src/main/java/com/sonbro/projectj/ProjectJApplication.java
@@ -0,0 +1,13 @@
+package com.sonbro.projectj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectJApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectJApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sonbro/projectj/ProjectJApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonbro.projectj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectJApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
